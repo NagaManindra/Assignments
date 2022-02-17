93040c5a7f83404ce90447b0b6a6237e673ef17f
Manindra <78266893+NagaManindra@users.noreply.github.com>
2022-02-17 07:51:12 +0530

    String Assignment Submitted

---
+++ Core Java/String Assignment/StringBuilderQ3.java
@@ -0,0 +1,11 @@
+
+public class StringBuilderQ3 {
+
+	public static void main(String[] args) {
+		StringBuilder string = new StringBuilder();
+		string.append("This method returns the reversed object on which it was called");
+		System.out.println(string.reverse());
+
+	}
+
+}
---
+++ Core Java/String Assignment/StringQ1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class StringQ1 {
+
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		String word = scanner.nextLine();
+		System.out.println("length of the String is "+word.length());
+		scanner.close();
+	}
+
+}
