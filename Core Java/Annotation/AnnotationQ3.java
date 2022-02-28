c0176d8494487eac29a19a1052c222977919e9a8
Manindra <78266893+NagaManindra@users.noreply.github.com>
2022-02-28 14:28:31 +0530

    Updated

--- Core Java/Annotation/AnnotationQ3.java
+++ Core Java/Annotation/AnnotationQ3.java
@@ -27,37 +27,98 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+
 
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 @interface  Execute{
-	int Sequence() default 12;
+ 	public int Sequence() default 1;
 }
+
 
 class MyClass{
 	@Execute(Sequence=2)
 	public void myMethod1(){
-		
+		System.out.println("Method 1 invoked");
 	}
+	
 	@Execute(Sequence=1)
 	public void myMethod2(){
-		
+		System.out.println("Method 2 invoked");
 	}
 	@Execute(Sequence=3)
 	public void myMethod3(){
-		
+		System.out.println("Method 3 invoked");
 	}
+	
 }
 public class AnnotationQ3 {
 
-	public static void main(String[] args) throws NoSuchMethodException, SecurityException {
+	public static void main(String[] args) {
 		MyClass objClass =new MyClass();
-		Class<? extends MyClass> c =objClass.getClass();
-		Method method=c.getMethod("myMethod3");
-		Execute annotation = method.getAnnotation(Execute.class);
-		System.out.println(((Execute) annotation).Sequence()); 
-
+		try {
+            Class<MyClass> annotatedClass = MyClass.class;
+            Method[] methods = annotatedClass.getDeclaredMethods();
+            for(int i=1;i<=3;i++) {
+	            for (Method method : methods) {
+	               if (method.isAnnotationPresent(Execute.class)) {
+	                  Execute sequence = method.getAnnotation(Execute.class);
+	                  int num = sequence.Sequence();
+	                  if (num==i) {
+	                	  method.invoke(objClass);
+	                     }
+	               }
+	            }
+            }
+         } catch (Exception e) {
+            
+         }
 	}
 
 }
+
+
+
+
+
+
+/**
+import java.lang.annotation.*;  
+import java.lang.reflect.*;  
+ 
+@Retention(RetentionPolicy.RUNTIME)  
+@Target(ElementType.METHOD)  
+@interface Execute{  
+int Sequence();
+}  
+@interface Order{
+	int value();
+}
+class Myclass{   
+@Order(value=1)
+@Execute(Sequence=2) 
+public void myMethod1(){System.out.println("one");}  
+@Order(value=2)
+@Execute(Sequence=1)
+public void myMethod2() {System.out.println("Two");}
+@Order(value=3)
+@Execute(Sequence=3)
+public void myMethod3() {System.out.println("Three");}
+}  
+public class AnnotationQ3 {
+	public static void main(String args[])throws Exception{  
+		  
+		Myclass h=new Myclass();  
+		Method m=h.getClass().getMethod("myMethod1");  
+		Method z=h.getClass().getMethod("myMethod2");
+		Method q=h.getClass().getMethod("myMethod3");  
+		Execute manno=m.getAnnotation(Execute.class);
+		Execute ma=z.getAnnotation(Execute.class);
+		Execute no=q.getAnnotation(Execute.class);
+		System.out.println(manno);  
+		System.out.println(ma);
+		System.out.println(no);
+		}}
+**/
